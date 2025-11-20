e09de2ca3d62a1a51708237e97003f7bdb05723e
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-11-20 21:21:07 +0100

    try catch added to persister execution

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class Main
 {
-    public static void main(String[] args)
+  private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+  public static void main(String[] args)
+  {
+    User u1 = new User(10, "Bob");
+    User u2 = new User(20, "Alice");
+    User u3 = new User(30, "Carl");
+    User u4 = new User(11, "Bob");
+
+    UserList list = new UserList();
+    list.add(u1);
+    list.add(u2);
+    list.add(u3);
+    list.add(u4);
+
+    System.out.println(list.getu());
+
+    try
     {
-        User u1 = new User(10, "Bob" );
-        User u2 = new User(20, "Alice");
-        User u3 = new User(30, "Carl");
-        User u4 = new User(11,"Bob");
-
-        UserList list = new UserList();
-        list.add(u1);
-        list.add(u2);
-        list.add(u3);
-        list.add(u4);
-
-        System.out.println(list.getu());
-
-        Persister.s(list.getu());
+      Persister.s(list.getu());
     }
+    catch (IOException e)
+    {
+      logger.severe("Failed to persist user list: " + e.getMessage());
+    }
+  }
 }
