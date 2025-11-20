0f6edbb65e44b59c761cf30e0658b9737742e85d
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-11-20 21:44:24 +0100

    better names in UserList

--- src/Persister.java
+++ src/Persister.java
@@ -3,15 +3,16 @@ import java.util.*;
 
 public class Persister
 {
-    public static void s(ArrayList<User> u) throws IOException
-    {
-        File file = new File("users.txt"); // Creates file
-        FileWriter fw = new FileWriter(file); // Create filewriter
-        BufferedWriter bw = new BufferedWriter(fw); // Create BufferedWriter
-        for(int x=0;x<u.size()-1;x++) // Create a loop
-        { // Nicely formatted block
-            bw.write(u.get(x).user_name); // Write first thing
-            bw.write(u.get(x).get()); // Write second thing
-        } // Ending parenthesis
-    }
+  public static void s(ArrayList<User> u) throws IOException
+  {
+    File file = new File("users.txt"); // Creates file
+    FileWriter fw = new FileWriter(file); // Create filewriter
+    BufferedWriter bw = new BufferedWriter(fw); // Create BufferedWriter
+    // Create a loop
+    for (User user : u)
+    { // Nicely formatted block
+      bw.write(user.user_name); // Write first thing
+      bw.write(user.get()); // Write second thing
+    } // Ending parenthesis
+  }
 }
